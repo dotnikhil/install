7436ceb4ac83610ccfbf9b2faf3edb4e890110db
dotnikhil <nikhilmaini1972@gmail.com>
2025-07-11 10:28:35 +0530

    Update complete.sh

--- complete.sh
+++ complete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo wget https://raw.githubusercontent.com/akshu20791/Deployment-script/main/jenkins.sh
+sudo wget https://raw.githubusercontent.com/dotnikhil/install/refs/heads/main/jenkins.sh
 sudo chmod 777 jenkins.sh
 sudo sh jenkins.sh
 sudo wget https://raw.githubusercontent.com/akshu20791/Deployment-script/main/k8s-master.sh
