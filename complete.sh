e1f4c5fa655d18a0671277b78c244e52f1416659
dotnikhil <nikhilmaini1972@gmail.com>
2025-07-11 10:26:42 +0530

    Create complete.sh

---
+++ complete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo wget https://raw.githubusercontent.com/akshu20791/Deployment-script/main/jenkins.sh
+sudo chmod 777 jenkins.sh
+sudo sh jenkins.sh
+sudo wget https://raw.githubusercontent.com/akshu20791/Deployment-script/main/k8s-master.sh
+sudo chmod 777 k8s-master.sh
+sudo sh k8s-master.sh
+sudo usermod -aG docker jenkins
+sudo cp /root/.kube/config .
+sudo chown -R jenkins:jenkins /home/config
